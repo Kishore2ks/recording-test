ad22c03f1b608933e6d829b19dd6f43784070e90
kishore <kishorekumar.loganathan@techmobius.com>
2025-07-18 16:34:00 +0530

    build 2

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: { port: 4200 },
+  server: {
+    port: 4200,
+    allowedHosts: ["kishore2ks.github.io", "localhost"],
+  },
   build: { outDir: "docs" },
 });
